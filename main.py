4c5ce81a3b81b2129d8e20e22cb72b2aab8c1fb5
Carmen <carmen.moreno.udaondo@gmail.com>
2021-08-28 11:35:12 +0200

    Basic user interface and complementary strand generator added

---
+++ main.py
@@ -0,0 +1,39 @@
+# color palette = https://paletasdecolores.com/paleta-de-colores-4264/
+from tkinter import *
+
+
+def complementary_strand():
+    button.configure(state='disabled')
+
+    dna_strand = original_entry.get().upper()
+    complementary_nucleobases = {"A": "T", "T": "A", "C": "G", "G": "C"}
+    adnc = ''.join([complementary_nucleobases[nucleobase] for nucleobase in dna_strand])
+
+    complementary_label = Label(root, text='Complementary DNA strand: ')
+    complementary_label.configure(bg='#bcb8ce')
+    complementary_label.grid(pady=10, padx=10, row=2, column=0)
+
+    complementary_entry = Entry(root, borderwidth=5, width=100)
+    complementary_entry.grid(pady=10, padx=10, row=2, column=1)
+    complementary_entry.insert(0, adnc)
+    complementary_entry.configure(state='disabled')
+
+
+root = Tk()
+root.title("Complementary strands")
+root.configure(bg='#bcb8ce')
+
+# Create Widgets
+original_label = Label(root, text='Enter the original DNA strand: ')
+original_label.configure(bg='#bcb8ce')
+original_label.grid(pady=10, padx=10, row=0, column=0)
+
+original_entry = Entry(root, borderwidth=5, width=100)
+original_entry.grid(pady=10, padx=10, row=0, column=1)
+
+
+button = Button(root, text='Generate complementary DNA strand', width=60, command=complementary_strand)
+button.grid(padx=15, pady=15, row=2, column=1, columnspan=2)
+
+
+root.mainloop()
